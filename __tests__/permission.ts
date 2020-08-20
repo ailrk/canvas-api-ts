9b767ccc4fa4d866a765d848a9ba4ce16eeb9173
Jimmy <jimmy123good@hotmail.com>
2020-08-20 14:55:31 -0700

    files

--- __tests__/permission.ts
+++ __tests__/permission.ts
@@ -1,5 +1,8 @@
 import {getUserPageViews} from '../src/wrapper/user';
 
+describe("Test actions need student permission with student permission", () => {
+});
+
 describe("Test actions need more than student permission with student permission",
   () => {
     it("Should filed beacuse of the student authorization", async () => {
