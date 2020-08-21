97796abf0fc7690abd1764ea14e583def07fa7c5
Jimmy <jimmy123good@hotmail.com>
2020-08-21 01:56:35 -0700

    change blocking code

--- .ccls-cache/@home@jimmy@newDisk@Repo@canvas-api-ts/ass@A2_Q3.c
+++
@@ -1,2 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<Error><Code>InvalidArgument</Code><Message>Only one auth mechanism allowed; only the X-Amz-Algorithm query parameter, Signature query string parameter or the Authorization header should be specified</Message><ArgumentName>Authorization</ArgumentName><ArgumentValue>Bearer 11224~7jh4ElSYbmNKYG4N3W0dKpu5AEddTHfyzHMb6x7CdpMMSvMruN3HL7xAokxQ9mYC</ArgumentValue><RequestId>62DC8D913EB31BEB</RequestId><HostId>dVTnkUWcmO1J9BtsCOBCUsebPSa606CbNvQ+jxGWM7N8QcmVL5SJiz7nemmpFd0XSg87PBwQNOE=</HostId></Error>
